5cf57407c194a3c59859ce6ff28beefa14a0c2d0
Mauricio Robayo <rfmajo@gmail.com>
2025-03-29 10:34:42 -0500

    tree styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import { ciiuService, type CiiuData } from "@/app/ciiu/service";
 import {
   Box,
-  Card,
   Code,
   Container,
   Flex,
-  Heading,
   Link,
   Section,
   Text,
+  Heading,
 } from "@radix-ui/themes";
 import NextLink from "next/link";
+import { twJoin } from "tailwind-merge";
 
 export default async function Page() {
   const ciiuTree = await ciiuService.getTree();
@@ -23,56 +23,101 @@ export default async function Page() {
   );
 }
 
-const LevelMap: Record<number, string> = {
-  1: "Divisiones",
-  2: "Grupos",
-  3: "Clases",
+const LevelMap: Record<
+  number,
+  {
+    singular: string;
+    plural: string;
+    cn: { group: string; rotate: string; font: string };
+  }
+> = {
+  1: {
+    singular: "División",
+    plural: "Divisiones",
+    cn: {
+      group: "group/divisions",
+      rotate: "group-open/divisions:rotate-90",
+      font: "group-open/divisions:font-bold",
+    },
+  },
+  2: {
+    singular: "Grupo",
+    plural: "Grupos",
+    cn: {
+      group: "group/groups",
+      rotate: "group-open/groups:rotate-90",
+      font: "group-open/groups:font-bold",
+    },
+  },
+  3: {
+    singular: "clase",
+    plural: "Clases",
+    cn: {
+      group: "group/classes",
+      rotate: "group-open/classes:rotate-90",
+      font: "group-open/classes:font-bold",
+    },
+  },
 };
 
 function Tree({ tree, level = 1 }: { tree: CiiuData[]; level?: number }) {
+  const levelData = LevelMap[level];
   return (
-    <Flex direction="column" asChild>
-      <ul className={level > 3 ? "" : "divide-[var(--gray-6)] divide-y"}>
-        {tree.map((section) => (
-          <li key={section.code}>
-            <Card variant="ghost" mx="4" my={level > 3 ? "0" : "2"}>
-              <Flex gap="2" direction="column">
-                <Flex direction="row" align="center" gap="2">
-                  <Code variant="solid">{section.code}</Code>
-                  <Heading
-                    as="h2"
-                    size={{ initial: "1", sm: "2" }}
-                    weight="regular"
-                    className="line-clamp-1"
-                  >
-                    {level > 3 ? (
-                      <Link asChild>
-                        <NextLink href={`./${section.code}`} prefetch={false}>
+    <ul className={level > 3 ? "" : "divide-[var(--gray-6)] divide-y"}>
+      {tree.map((section) => (
+        <li key={section.code}>
+          {section.children?.length ? (
+            <Flex mx="2" my="2" direction="column" asChild>
+              <details className={twJoin("cursor-pointer", levelData.cn.group)}>
+                <summary className="list-none [&::-webkit-details-marker]:hidden">
+                  <Flex direction="column" gap="1">
+                    <Flex direction="row" align="center" gap="2">
+                      <Code variant="solid">{section.code}</Code>
+                      <Heading
+                        size={{ initial: "1", sm: "2" }}
+                        className="line-clamp-1"
+                        as="h2"
+                        weight="regular"
+                      >
+                        <Text className={levelData.cn.font}>
                           {section.description}
-                        </NextLink>
-                      </Link>
-                    ) : (
-                      section.description
-                    )}
-                  </Heading>
-                </Flex>
-                {section.children?.length ? (
-                  <details className="open:font-bold">
-                    <summary>
-                      <Text size="2" color="gray">
-                        {LevelMap[level]}: {section.children.length}
+                        </Text>
+                      </Heading>
+                    </Flex>
+                    <Flex align="center" gap="1">
+                      <div
+                        className={`transition-transform ${levelData.cn.rotate}`}
+                      >
+                        &rarr;
+                      </div>
+                      <Text size="2">
+                        {section.children.length}{" "}
+                        {section.children.length > 1
+                          ? levelData.plural
+                          : levelData.singular}
                       </Text>
-                    </summary>
-                    <Box my="2">
-                      <Tree tree={section.children} level={level + 1} />
-                    </Box>
-                  </details>
-                ) : null}
-              </Flex>
-            </Card>
-          </li>
-        ))}
-      </ul>
-    </Flex>
+                    </Flex>
+                  </Flex>
+                </summary>
+                <Box my="2">
+                  <Tree tree={section.children} level={level + 1} />
+                </Box>
+              </details>
+            </Flex>
+          ) : (
+            <Flex direction="row" align="center" gap="2" my="1" mx="2">
+              <Code variant="solid">{section.code}</Code>
+              <Text size={{ initial: "1", sm: "2" }} className="line-clamp-1">
+                <Link asChild>
+                  <NextLink href={`./${section.code}`} prefetch={false}>
+                    {section.description}
+                  </NextLink>
+                </Link>
+              </Text>
+            </Flex>
+          )}
+        </li>
+      ))}
+    </ul>
   );
 }
